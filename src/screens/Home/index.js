0abf0d75e9271e893a82ad823ebeccfae717fb64
Sergey <master@peepo.club>
2021-01-23 18:50:01 +0700

    Oh

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -6,7 +6,7 @@ function Home() {
       <header className="text-gray-600 body-font">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <div className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <img className="h-10" src="/assets/logo.svg" />
+            <img className="h-10" alt="logo" src="/assets/logo.svg" />
             <span className="ml-3 text-xl">peepoChat</span>
           </div>
           <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
